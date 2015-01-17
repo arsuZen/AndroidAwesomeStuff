0c765b30c4b2d35ec59a7962826e3b467fd9b9fd
asu <suckasu69@gmail.com>
2015-01-17 18:28:10 +0800

    Remove TextView & remove not used import stuff

--- app/src/main/java/androidawesome/asu/squareface/androidawesomestuff/MainActivity.java
+++ app/src/main/java/androidawesome/asu/squareface/androidawesomestuff/MainActivity.java
@@ -1,10 +1,7 @@
 package androidawesome.asu.squareface.androidawesomestuff;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.Toast;
 
 
@@ -12,15 +9,14 @@ public class MainActivity extends ActionBarActivity {
 
     private static String TAG = "mTag";
     private static String TAG_SF = "squareface";
-//Testcomment
-    private TextView tvHello;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"Oh! yeah!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Oh! yeah!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
