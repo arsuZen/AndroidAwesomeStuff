6c75886d29cae5c80d9ee5b6acd18b321f7eeac9
asu <suckasu69@gmail.com>
2015-01-17 11:48:34 +0800

    Add textView ID

--- app/src/main/java/androidawesome/asu/squareface/androidawesomestuff/MainActivity.java
+++ app/src/main/java/androidawesome/asu/squareface/androidawesomestuff/MainActivity.java
@@ -1,10 +1,8 @@
 package androidawesome.asu.squareface.androidawesomestuff;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -12,12 +10,14 @@ public class MainActivity extends ActionBarActivity {
 
     private static String TAG = "mTag";
 
+    private TextView tvHello;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"Oh! yeah!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Oh! yeah!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
